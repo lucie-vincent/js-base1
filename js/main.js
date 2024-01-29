05122b13bb78ceebebf8b37c3ae5a862f88fddce
Lucie Vincent <lucie.sarah.vincent@gmail.com>
2024-01-29 09:52:35 +0100

    ajout méthode alert() + tentative console.log

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 const carre = document.querySelector(".carre");
-const carreInfos = carre.value;
+// const carreInfos = carre.value;
 
 function afficherInfos() {
-    alert(`${console.log(carreInfos)}`);
+    // alert(`${console.log(carreInfos)}`);
+    alert("Class: carre\n -Background color: rgb(47, 79, 79)\n -Height: 200\n -Width: 200\n -Display: flex\n -Display: \"Times New Roman\"(40px)");
 }
 
 carre.addEventListener('click', afficherInfos);
