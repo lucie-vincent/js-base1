0dc67b6fe6c3be7ae545373c94f3576112c0da9d
Lucie Vincent <lucie.sarah.vincent@gmail.com>
2024-01-29 11:44:29 +0100

    récupérer classe

--- js/main.js
+++ js/main.js
@@ -1,21 +1,8 @@
 let carre = document.querySelector(".carre");
-// const carreInfos = carre.value;
 let carreStyle = window.getComputedStyle(carre, null);
 
-/*afficher les infos
-class :
-background color : carreStyle.backgroundColor
-color : carreStyle.color
-height : carreStyle.height
-width : carreStyle.width
-display : carreStyle.display
-police : carreStyle.font
-
-*/ 
-
-
 function afficherInfos() {
-    alert(`Class : \n
+    alert(`Class : ${carre.className}\n
     - BackgroundColor : ${carreStyle.backgroundColor}:\n
     - Color : ${carreStyle.color}\n
     - Height : ${carreStyle.height}\n
@@ -24,7 +11,6 @@ function afficherInfos() {
     - Police : ${carreStyle.font}\n
     `);
 }
-
 
 carre.addEventListener('click', afficherInfos);
 
